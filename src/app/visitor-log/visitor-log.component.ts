ae9c7c0fe2a1961e8d13bf48d3f485190a5dc78f
Amirtha M Lalas <Amirtha@NTP-LAP-746.chn.nestgroup.net>
2022-12-13 14:09:33 +0530

    added visitorlog

---
+++ src/app/visitor-log/visitor-log.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
+
+@Component({
+  selector: 'app-visitor-log',
+  templateUrl: './visitor-log.component.html',
+  styleUrls: ['./visitor-log.component.css']
+})
+export class VisitorLogComponent {
+
+  name=""
+ purpose=""
+  whom_to_meet=""
+   entry_time=""
+  exit_time=""
+constructor(private api:ApiService){}
+  readValue=()=>{
+    let data:any={
+      "name":this.name,"purpose":this.purpose,"whom_to_meet":this.whom_to_meet,"entry_time":this.entry_time,"exit_time":this.exit_time
+    }
+this.api.visitorLog(data).subscribe(
+  (response:any)=>{
+    if (response.status=="success") {
+      alert("working")
+      
+    } else {
+      alert("FAIL")
+    }
+  }
+)
+    
+  }
+
+}
